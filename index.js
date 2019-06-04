459ff5f37073621dccc3a575926bcb6f824eaa62
cheshireCode <dac4158@gmail.com>
2019-06-04 15:05:48 +0800

    cover Node 8 older than 8.5 just in case

--- index.js
+++ index.js
@@ -1,24 +1,26 @@
 /** global window */
 
-module.exports = (timeout = 10000) => {
+const getTsNow = require("./getTsNow");
+module.exports = (timeout = 10000, cb) => {
   if (typeof window === "undefined") window = {}; //eslint-disable-line no-global-assign
-  if (typeof performance === "undefined")
-    performance = require("perf_hooks").performance; //eslint-disable-line no-global-assign
-  let start = window.__NOW__ || performance.now(); // eslint-disable-line no-underscore-dangle
+  let start = window.__NOW__ || getTsNow(); // eslint-disable-line no-underscore-dangle
   let t;
-  let cb = payload => {
-    const timing = performance.now() - start;
+  let measure = () => {
+    const timing = getTsNow() - start;
+    clearTimeout(t);
     /* istanbul ignore if */
     if (window.__NOW__) {
       window.__NOW__ = 0; // eslint-disable-line no-underscore-dangle
     }
-    clearTimeout(t);
+    const cbType = cb ? cb.toString() : "";
     /* istanbul ignore if */
-    if (window.dataLayer) {
-      window.dataLayer.push({
-        ...payload,
-        timingValue: timing
-      });
+    if (
+      cbType == "[object Function]" ||
+      cbType == "[object AsyncFunction]" ||
+      cbType == "[object GeneratorFunction]" ||
+      cbType == "[object Proxy]"
+    ) {
+      return cb(timing);
     }
     return timing;
   };
@@ -26,12 +28,12 @@ module.exports = (timeout = 10000) => {
   /* istanbul ignore next */
   t = setTimeout(() => {
     start = "";
-    cb = "";
+    measure = "";
     /* istanbul ignore if */
     if (window.__NOW__) {
       window.__NOW__ = 0; // eslint-disable-line no-underscore-dangle
     }
   }, timeout);
 
-  return cb;
+  return measure;
 };
--- index.test.js
+++ index.test.js
@@ -1,53 +1,29 @@
 const measure = require("./");
 const test = require("tape");
 
-test("measure", async t => {
-  let m = measure();
-  (() => {})();
+test("measure directly", async t => {
+  await Promise.all(
+    [1, 1000, 500, 2000].map(
+      milliseconds =>
+        new Promise(async r => {
+          const m = measure();
+          await new Promise(resolve => {
+            const wait = setTimeout(() => {
+              clearTimeout(wait);
+              resolve();
+            }, milliseconds);
 
-  t.equal(m() > 0.001, true, "noop takes around 0.001 milliseconds");
-
-  await Promise.all([
-    new Promise(async r => {
-      m = measure();
-      await new Promise(resolve => {
-        const wait = setTimeout(() => {
-          clearTimeout(wait);
-          resolve();
-        }, 1000);
-
-        return wait;
-      });
-      t.equal(m() > 1000, true, "something that takes 1000 milliseconds");
-      r();
-    }),
-    new Promise(async r => {
-      m = measure();
-      await new Promise(resolve => {
-        const wait = setTimeout(() => {
-          clearTimeout(wait);
-          resolve();
-        }, 500);
-
-        return wait;
-      });
-      t.equal(m() > 500, true, "something that takes 500 milliseconds");
-      r();
-    }),
-    new Promise(async r => {
-      m = measure();
-      await new Promise(resolve => {
-        const wait = setTimeout(() => {
-          clearTimeout(wait);
-          resolve();
-        }, 2000);
-
-        return wait;
-      });
-      t.equal(m() > 2000, true, "something that takes 2000 milliseconds");
-      r();
-    })
-  ]);
+            return wait;
+          });
+          t.equal(
+            m() > milliseconds - 1,
+            true,
+            `something that takes ${milliseconds} milliseconds`
+          );
+          r();
+        })
+    )
+  );
 
   t.end();
 });
